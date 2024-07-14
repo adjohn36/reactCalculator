6f50dd4f5053940b3eba625815868ba1464b78fc
adjohn36 <132848782+adjohn36@users.noreply.github.com>
2024-07-13 23:06:50 -0400

    Add files via upload

---
+++ react calculator/App.js
@@ -0,0 +1,13 @@
+function App() {
+    return (
+      <div className="calculator-grid">
+        <div className="output">
+            <div className="previous-operand"></div>
+            <div className="current-operand"></div>
+        </div>
+        <button className="span-two">AC</button>
+      </div>  
+    )
+}
+
+export default App;
